c9336460a1fcb36040617705955a23f9749789b1
ayning <1012706684@qq.com>
2015-08-06 14:54:47 +0800

    新增

---
+++ AjaxRequest.java
@@ -0,0 +1,25 @@
+public Class AjaxRequest {
+	
+	/**
+	* 判断请求是否是Ajax请求
+	*/
+	public boolean isAjax(HttpServletRequest request, HttpServletResponse response) {
+		String requestType = request.getHeader("X-Requested-With");
+		if (requestType != null && requestType.equalsIgnoreCase("XMLHttpRequest")) {
+			response.addHeader("loginStatus", "accessDenied");
+			response.sendError(HttpServletResponse.SC_FORBIDDEN);
+			return false;
+		} else {
+			if (request.getMethod().equalsIgnoreCase("GET")) {
+				String redirectUrl = request.getQueryString() != null ? request.getRequestURI() + "?" + request.getQueryString() : request.getRequestURI();
+				response.sendRedirect(loginUrl + "&" + REDIRECT_URL_PARAMETER_NAME + "=" + URLEncoder.encode(redirectUrl, urlEscapingCharset));
+			} else {
+				response.sendRedirect(loginUrl);
+			}
+			return false;
+		}
+		
+	}
+	
+	
+}
